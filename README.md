9d5c07187cf1c1e9ce5e65b8f28b2882093f927e
Theron Boerner <hunterboerner@gmail.com>
2015-12-24 13:05:14 -0600

    Aquamacs

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ More information and screenshots will be added later. If you have questions,
 please create an issue. Pull requests are welcome!
 
 > brew install --HEAD michaeldfallen/formula/git-radar
+
+To use Aquamacs, link:
+
+  - /Users/theron/Library/Aquamacs\ Emacs/Preferences.el to emacs/.emacs.d/Preferences.el
+  - /Users/theron/Library/Aquamacs\ Emacs/customizations.el to emacs/.emacs.d/customizations.el
+  - /Users/theron/Library/Aquamacs\ Emacs/Packages/Cask to emacs/.emacs.d/Cask
---
+++ emacs/.emacs.d/customizations.el
@@ -0,0 +1,97 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-fold-auto t)
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   ["#151515" "#fb9fb1" "#acc267" "#ddb26f" "#6fc2ef" "#e1a3ee" "#6fc2ef" "#d0d0d0"])
+ '(ansi-term-color-vector
+   [unspecified "#181818" "#ab4642" "#a1b56c" "#f7ca88" "#7cafc2" "#ba8baf" "#7cafc2" "#d8d8d8"] t)
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-autoface-mode nil)
+ '(aquamacs-customization-version-id 308 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(column-number-mode t)
+ '(create-lockfiles nil)
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("f024aea709fb96583cf4ced924139ac60ddca48d25c23a9d1cd657a2cf1e4728" "50e7f9d112e821e42bd2b8410d50de966c35c7434dec12ddea99cb05dd368dd8" "f21caace402180ab3dc5157d2bb843c4daafbe64aadc362c9f4558ac17ce43a2" "cdfb22711f64d0e665f40b2607879fcf2607764b2b70d672ddaa26d2da13049f" "0bd7a42bd443517e5e61dac3cabc24018fbd0c6b2b4199b3c4efd9e3727efd30" "b6db49cec08652adf1ff2341ce32c7303be313b0de38c621676122f255ee46db" "d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" "f5eb916f6bd4e743206913e6f28051249de8ccfd070eae47b5bde31ee813d55f" "11636897679ca534f0dec6f5e3cb12f28bf217a527755f6b9e744bd240ed47e1" "19352d62ea0395879be564fc36bc0b4780d9768a964d26dfae8aad218062858d" "17f35b689dd41e49cb740bfb810ac8a53d13292cbebf68f41f772787d8b3aebf" "3514fe07ecd0dc2f8b747706370c169b3d8053f825c009414e1a86eac8978327" "1dd2d01010a9ae1f54775abceb080e231b6f9c781c5282b25b8c4edd3f3a14e0" "3f04f37604c5f5cc3c71bc1a4a604ed8be340d0f150946b25658e403ccbad6d2" "d7e434a3c19f87fa00b945edfaedc9a21a6e436a7814c23277d4112ad83b5e85" "d72836155cd3b3e52fd86a9164120d597cbe12a67609ab90effa54710b2ac53b" "6ebb2401451dc6d01cd761eef8fe24812a57793c5ccc427b600893fa1d767b1d" "d3a86848a5e9bf123f3dd3bf82ab96995837b50f780dd7d5f65dc72c2b81a955" "9fab7d4f70b99927d898a0c93b1ae90fc1529248b9c2604ed88e864fc2665aa7" "95a6ac1b01dcaed4175946b581461e16e1b909d354ada79770c0821e491067c6" "a2e7b508533d46b701ad3b055e7c708323fb110b6676a8be458a758dd8f24e27" "b06aaf5cefc4043ba018ca497a9414141341cb5a2152db84a9a80020d35644d1" "9e7cc7f1db252d6fe0c7cc25d72d768712a97cda1f77bf63f0c1fd7be5dee7f4" "4ff23437b3166eeb7ca9fa026b2b030bba7c0dfdc1ff94df14dfb1bcaee56c78" "f0e69da2cf73c7f153fc09ed3e0ba6e1fd670fec09b8a6a8ed7b4f9efea3b501" "9f3a4edb56d094366afed2a9ba3311bbced0f32ca44a47a765d8ef4ce5b8e4ea" "ced74ff794aad9ac93266bf9a9a92c5641c01b05715c6862e30459a24844eec9" "3328e7238e0f6d0a5e1793539dfe55c2685f24b6cdff099c9a0c185b71fbfff9" "113ae6902d98261317b5507e55ac6e7758af81fc4660c34130490252640224a2" "6dbd0dd4c344f1ca534422cc5a1fd3ed822dcde947ae983948b70c7284a0ed33" "3dafeadb813a33031848dfebfa0928e37e7a3c18efefa10f3e9f48d1993598d3" "83279c1d867646c5eea8a804a67a23e581b9b3b67f007e7831279ed3a4de9466" "e7ec0cc3ce134cc0bd420b98573bbd339a908ac24162b8034c98e1ba5ee1f9f6" "1abda075ebacaa3795d675bb2be0a905322ac856f9c0c259da63f9ccfe1962ec" "75c0b1d2528f1bce72f53344939da57e290aa34bea79f3a1ee19d6808cb55149" "3539b3cc5cbba41609117830a79f71309a89782f23c740d4a5b569935f9b7726" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "cbef37d6304f12fb789f5d80c2b75ea01465e41073c30341dc84c6c0d1eb611d" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
+ '(default-frame-alist
+    (quote
+     ((tool-bar-lines . 0)
+      (menu-bar-lines . 1)
+      (background-color . "White")
+      (cursor-type . box)
+      (cursor-color . "Red")
+      (internal-border-width . 0)
+      (fringe)
+      (left-fringe . 1)
+      (right-fringe . 1))))
+ '(ede-project-directories (quote ("/Users/theron/code/hunterboerner/cpp-class")))
+ '(editorconfig-mode t)
+ '(fci-rule-color "#202325")
+ '(fill-column 80)
+ '(fringe-mode (quote (1 . 1)) nil (fringe))
+ '(global-company-mode t)
+ '(global-prettify-symbols-mode t)
+ '(haskell-tags-on-save t)
+ '(helm-quick-update t)
+ '(inhibit-startup-screen t)
+ '(magit-push-always-verify nil)
+ '(magit-revert-buffers nil)
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode nil t)
+ '(package-selected-packages
+   (quote
+    (ac-js2 ace-jump-mode ag alchemist base16-theme cider clojure-mode coffee-mode color-theme color-theme-sanityinc-tomorrow comment-dwim-2 company-auctex company-c-headers company-ghc company-tern dash-functional diff-hl disaster discover-js2-refactor editorconfig elixir-mode elixir-yasnippets ember-yasnippets emmet-mode emr erlang evil-nerd-commenter exec-path-from-shell expand-region fish-mode flatland-theme flx-ido flycheck-pyflakes flycheck-rust flymake-easy flymake-elixir ghc gist go-mode grizzl haskell-mode helm-ag helm-flx helm-gtags highlight-chars highlight-indentation hlinum ibuffer-vc impatient-mode js2-refactor json-mode json-reformat list-utils llvm-mode magit magit-popup makey markdown-mode material-theme multiple-cursors neotree ninja-mode nlinum pallet paradox paredit pcache popup projectile protobuf-mode purescript-mode python-mode queue racer rainbow-delimiters redshank robe rust-mode rvm s sass-mode scss-mode shm ember-mode zenburn-theme yasnippet yard-mode yaml-mode yafolding with-editor web-mode volatile-highlights undo-tree tuareg toml-mode tern spacegray-theme snippet smooth-scrolling smex smartparens skewer-mode shut-up)))
+ '(paradox-github-token t)
+ '(py-shell-name "ipython")
+ '(safe-local-variable-values
+   (quote
+    ((flycheck-clang-language-standard . c++14)
+     (erlang-indent-level . 2)
+     (py-which-shell . "python3"))))
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(vc-annotate-background "#1f2124")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#ff0000")
+     (40 . "#ff4a52")
+     (60 . "#f6aa11")
+     (80 . "#f1e94b")
+     (100 . "#f5f080")
+     (120 . "#f6f080")
+     (140 . "#41a83e")
+     (160 . "#40b83e")
+     (180 . "#b6d877")
+     (200 . "#b7d877")
+     (220 . "#b8d977")
+     (240 . "#b9d977")
+     (260 . "#93e0e3")
+     (280 . "#72aaca")
+     (300 . "#8996a8")
+     (320 . "#afc4db")
+     (340 . "#cfe2f2")
+     (360 . "#dc8cc3"))))
+ '(vc-annotate-very-old-color "#dc8cc3")
+ '(visual-line-mode nil t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Source Code Pro"))))
+ '(elixir-ignored-var-face ((t (:foreground "dark gray"))))
+ '(font-lock-keyword-face ((t (:foreground "#F0DFAF" :weight normal))))
+ '(linum-highlight-face ((t (:inherit default :weight bold))))
+ '(minibuffer-inactive-mode-default ((t (:inherit autoface-default))) t)
+ '(text-mode-default ((t (:inherit autoface-default)))))
