434a4bedeca5c3aa63fec6795b3c3bc51d34eea2
Theron Boerner <hunterboerner@gmail.com>
2021-07-23 19:28:03 -0700

    update fish config for gcloud

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,4 +9,5 @@ source $OMF_PATH/init.fish
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 source (brew --prefix asdf)/asdf.fish
+source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
 set -g fish_user_paths "/usr/local/opt/opencv@3/bin" $fish_user_paths
